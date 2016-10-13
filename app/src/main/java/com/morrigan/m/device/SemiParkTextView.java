352e0767ae5687a45ef065c806a6e7c29b5ddf90
yzeaho <y-zeaho@qq.com>
2016-10-14 01:09:10 +0800

    hiden main right button

--- app/src/main/java/com/morrigan/m/device/SemiParkTextView.java
+++ app/src/main/java/com/morrigan/m/device/SemiParkTextView.java
@@ -3,7 +3,6 @@ package com.morrigan.m.device;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
@@ -13,7 +12,6 @@ import android.widget.TextView;
  */
 public class SemiParkTextView extends TextView {
 
-    private RectF oval = new RectF();
     private Paint paint;
 
     public SemiParkTextView(Context context, AttributeSet attrs) {
@@ -25,12 +23,7 @@ public class SemiParkTextView extends TextView {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        oval.left = 0;
-        oval.top = getTop();
-        oval.right = getWidth();
-        oval.bottom = getHeight();
         canvas.drawCircle(getWidth() / 2, getHeight(), getWidth() / 2, paint);
-//        canvas.drawArc(oval, 225, 90, false, paint);
         super.onDraw(canvas);
     }
 }
